fbc26bb3b0ae8c691c3af594b008c364115927d3
vien1994 <vien1994@yahoo.com>
2019-03-03 13:58:53 -0800

    mine

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function repeatHello() {
+    for (var i = 10; i >=0; i--) {
+        console.log("Hello World");
+    }
+}
+
+repeatHello();
+
+function consoleLogChange() {
+    console.log(document.getElementById("inputChange").value);
+}
+
+function doAddition() {
+    console.log(
+        parseInt(document.getElementById("var1").value) + parseInt(document.getElementById("var2").value)
+    );
+}
